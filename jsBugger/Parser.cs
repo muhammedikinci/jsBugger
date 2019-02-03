c8e92b0782af525ffecf7aceeb08bc24ac96c6ba
muhammedikinci <muhammedikinci@outlook.com>
2019-02-03 19:07:48 +0300

    initial commit

--- jsBugger/Parser.cs
+++ jsBugger/Parser.cs
@@ -7,19 +7,24 @@ namespace Core.Parse
     public class Parser
     {
         List<Token> TokenList = new List<Token>();
-        public static string currentFunctionName = "";
         int currentLine = 0;
         static bool inDQuote = false;
-        static bool inComment = false;
-        public static bool inFunction = false;
-        public static bool inDoubleFunction = false;
+        
         static bool inObject = false;
         static bool lastNewLine = false;
         static bool inStatement = false;
         static bool inAttributeStatements = false;
 
         static int returnCounter = 0;
+
         public static int lastAppenedFunctionCounter = 0;
+        static bool inComment = false;
+
+        public static string currentFunctionName = "";
+        public static bool inFunction = false;
+        public static bool inDoubleFunction = false;
+
+        public static int TabCount = 0;
 
         static List<Variable> Variables = new List<Variable>();
 
@@ -32,193 +37,85 @@ namespace Core.Parse
             {
                 Token CurrentToken = TokenList[i];
 
-                // -Start- Kullanılmayan değişken kontrolü
-                foreach (var item in Variables)
-                {
-                    if (EqualTokenString(i, item.variableName))
-                    {
-                        item.usedCounter++;
-                    }
-                }
-
-                if (EqualTokenType(i, WORD.RBRACE) && EqualTokenType(i + 1, WORD.COMMA) && inFunction)
-                {
-                    foreach (var item in Variables)
-                    {
-                        if (item.usedCounter < 1 && item.variableName != "data")
-                        {
-                            WriteError(i, "Kullanılmayan değişken -> " + item.variableName);
-                        }
-                    }
-
-                    Variables = new List<Variable>();
-                    if (inDoubleFunction)
-                    {
-                        inDoubleFunction = false;
-                    }
-                    else
-                    {
-                        inFunction = false;
-                    }
-                    returnCounter = 0;
-                    lastAppenedFunctionCounter = 0;
-                }
-                // -End- Kullanılmayan değişken kontrolü
-
-                // -Start- Yorum satırlarını es geç
-                if (EqualTokenType(i, WORD.COMMENTEND))
-                {
-                    inComment = false;
-                }
-
-                if (inComment)
-                {
-                    continue;
-                }
-
-                if (EqualTokenType(i, WORD.COMMENTSTART) && (EqualTokenType(i + 1, WORD.ASTERISK) || EqualTokenType(i + 2, WORD.ASTERISK)))
+                #region Comment
+                if (EqualTokenString(i, "/*") && !inComment)
                 {
                     inComment = true;
                 }
-                // -End- Yorum satırlarını es geç
+                #endregion
 
-                // -Start- Fonksiyon ismi al
-                if (EqualTokenType(i, WORD.FUNCTION) && EqualTokenType(i - 1, WORD.DOTT))
+                if (!inComment)
                 {
-                    if (!inFunction)
+                    #region Function
+                    if (EqualTokenType(i, WORD.FUNCTION) && EqualTokenType(i - 1, WORD.DOTT) && EqualTokenType(TokenList.Count - 1, WORD.LBRACE) && !inFunction)
+                    {
+                        inFunction = true;
+                        currentFunctionName = getPrevToken(i - 1).TokenString;
+                        TabCount++;
+                    }
+                    #endregion
+
+                    #region Double Function
+                    if (EqualTokenType(i, WORD.FUNCTION) && EqualTokenType(i + 2, WORD.LPARENT) && EqualTokenType(TokenList.Count - 1, WORD.LBRACE))
                     {
                         inDoubleFunction = true;
+                        TabCount++;
                     }
 
-                    currentFunctionName = TokenList[i - 2].TokenString;
-                    if (char.IsUpper(currentFunctionName[0]))
+                    if (inFunction)
                     {
-                        WriteError(i, "Fonksiyon isimleri küçük harf ile başlamalıdır.");
-                    }
-                    inFunction = true;
-                    lastAppenedFunctionCounter = 0;
-                } 
-                else if (EqualTokenType(i, WORD.FUNCTION) && EqualTokenType(i + 1, WORD.USERINPUT))
-                {
-                    inDoubleFunction = true;
-
-                    currentFunctionName = TokenList[i + 1].TokenString;
-                    if (char.IsUpper(currentFunctionName[0]))
-                    {
-                        WriteError(i, "Fonksiyon isimleri küçük harf ile başlamalıdır.");
-                    }
-                    inFunction = true;
-                    lastAppenedFunctionCounter = 0;
-                }
-                // -End- Fonksiyon ismi al
-
-                // -Start- Tek tırnak kontrolü
-                if (EqualTokenType(i, WORD.QUOTE) && !inDQuote)
-                {
-                    WriteError(i, "Stringler tek tırnak ile oluşturulmalıdır.");
-                }
-
-                if (EqualTokenType(i, WORD.DQUOTE) && inDQuote)
-                {
-                    inDQuote = false;
-                }
-                else if (EqualTokenType(i, WORD.DQUOTE) && !inDQuote)
-                {
-                    inDQuote = true;
-                }
-                // -End- Tek tırnak kontrolü
-
-                // -Start- Tek satırda birden fazla değişken kontrolü
-                if (EqualTokenType(i, WORD.VAR) && EqualTokenType(i + 2, WORD.COMMA))
-                {
-                    WriteError(i, "Tek satırda birden fazla değişken tanımlaması yapılmamalıdır.");
-                }
-                // -End- Tek satırda birden fazla değişken kontrolü
-
-                // -Start- Değişken tanımlamalarını tut.
-                if (EqualTokenType(i, WORD.VAR) && EqualTokenType(i + 1, WORD.USERINPUT) && inFunction)
-                {
-                    Variables.Add(new Variable() { usedCounter = 0, variableName = getNextToken(i).TokenString });
-                    i += 1;
-                }
-                // -End- Değişken tanımlamalarını tut.
-
-                // -Start- Return den önce boşlukları kontrol et
-                if (EqualTokenType(i, WORD.RETURN))
-                {
-                    if (lastAppenedFunctionCounter > 4 && inFunction && !inStatement)
-                    {
-                        if (!lastNewLine && currentFunctionName != "_testPageConditions")
+                        if (EqualTokenType(i, WORD.RETURN) && !inDoubleFunction && currentFunctionName != "sliderSettings" && currentFunctionName != "categorySettings")
                         {
-                            WriteError(i, " 'return' ifadelerinden önce boşluk bırakılmalı.");
+                            returnCounter++;
+                        }
+
+                        if (returnCounter > 1)
+                        {
+                            WriteError(i, "Birden fazla return kullanımı yanlış");
+                            returnCounter = 0;
                         }
                     }
-
-                    if (!inDoubleFunction && currentFunctionName != "isElementInSlider" &&
-                        currentFunctionName != "getCategoryList" && currentFunctionName != "_testPageConditions")
+                    else
                     {
-                        returnCounter++;
+                        returnCounter = 0;
                     }
 
-                    if (returnCounter > 1)
+                    if (EqualTokenType(i, WORD.RBRACE) && inDoubleFunction && TokenList.Count == 1 && TabCount == 2)
                     {
-                        WriteError(i, "Birden fazla return kullanımı yanlış.");
+                        TabCount--;
+                        inDoubleFunction = false;
                     }
-                }
-                // -End- Return den önce boşlukları kontrol et
+                    #endregion
 
-                // -Start- Attr - Prop fallback kontrol et
-                if (inAttributeStatements && EqualTokenType(i, WORD.COMMA))
-                {
-                    inAttributeStatements = false;
-                }
-
-                if (inAttributeStatements && EqualTokenType(i, WORD.RPARENT) && (EqualTokenType(i + 1, WORD.END) || EqualTokenType(i + 1, WORD.COMMA)
-                    || EqualTokenType(i + 1, WORD.RPARENT)))
-                {
-                    WriteError(i + 1, "Fallback gerekiyor.");
-                    inAttributeStatements = false;
-                }
-
-                if ((EqualTokenType(i, WORD.ATTR)) && EqualTokenType(i + 1, WORD.LPARENT))
-                {
-                    inAttributeStatements = true;
-                }
-                // -End- Attr - Prop fallback kontrol et
-
-                // -Start- Blokların bitişlerini takip et.
-                if (EqualTokenType(i, WORD.RBRACE))
-                {
-                    if (EqualTokenType(i + 1, WORD.END) && inObject)
+                    #region Function
+                    if (EqualTokenType(i + 1, WORD.COMMA) && EqualTokenType(i, WORD.RBRACE) && inFunction)
                     {
-                        inObject = false;
+                        inFunction = false;
+                        currentFunctionName = "";
+                        TabCount--;
                     }
-                    else if (inStatement)
-                    {
-                        inStatement = false;
-                    }
-                }
-                // -End- Blokların bitişlerini takip et.
-
-                if (EqualTokenType(i, WORD.NEWLINE))
-                {
-                    lastNewLine = true;
-                }
-                else
-                {
-                    lastNewLine = false;
+                    #endregion
                 }
 
-                if (EqualTokenType(i, WORD.IF) || EqualTokenType(i, WORD.ELSE))
+                #region Comment
+                if (EqualTokenString(i, "*/") && inComment)
                 {
-                    inStatement = true;
+                    inComment = false;
                 }
+                #endregion
             }
         }
 
         public Token getPrevToken(int i)
         {
-            return TokenList[i - 1];
+            if (i < 0)
+            {
+                return new Token();
+            } 
+            else
+            {
+                return TokenList[i - 1];
+            }
         }
 
         public Token getNextToken(int i)
--- jsBugger/Program.cs
+++ jsBugger/Program.cs
@@ -11,24 +11,20 @@ namespace jsBugger
         {
             try
             {
-                var fileText = File.ReadAllLines("C:\\Users\\muham\\Desktop\\spApi.js");
+                var fileText = File.ReadAllLines("C:\\Users\\muham\\Desktop\\Untitled.js");
                 //var fileText = File.ReadAllLines(args[0]);
-
+                Parser p = new Parser();
                 for (int i = 0; i < fileText.Length; i++)
                 {
                     int emptyLine = fileText[i] == "" ? 1 : 0;
                     Tokenizer.GetTokenFromString(fileText[i], emptyLine);
-                    Parser p = new Parser();
+                    
                     p.StartParse(Tokenizer.Tokens, i + 1);
-                    if (Parser.inFunction)
-                    {
-                        Parser.lastAppenedFunctionCounter++;
-                    }
                     Tokenizer.Tokens = new System.Collections.Generic.List<Token>();
                 }
                 Console.WriteLine("");
                 Console.WriteLine("Finished.");
-                Console.ReadKey();
+                Console.ReadLine();
             }
             catch (Exception)
             {
